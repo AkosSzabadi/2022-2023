dbf37141439423463f6ad7197f145256041549ae
Ydabas <90307763+AkosSzabadi@users.noreply.github.com>
2022-09-13 11:53:33 +0200

    GitHub

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitHub
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            for (int i = 0; i <= 40; i++)
+            {
+                Console.Write($"{random.Next(-100, 100)} ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
